df19f949eae3e104cea3aa1e390eaa1a07d2f789
kball <kball@zendev.com>
2019-10-04 15:48:29 -0700

    First pass screenshotter utility

---
+++ screenshot-plugin/cli.js
@@ -0,0 +1,22 @@
+const screenshot = require('./screenshot')
+
+;(async function() {
+  await screenshot(
+    [
+      [
+        'writing/waterfall',
+        'Waterfall',
+        'What Software, Business, and the Chainsmokers have in common'
+      ]
+      // ['writing', "swyx's Blogposts and Writing"],
+      // ['speaking', "swyx's Talks and Podcasts"]
+      // [
+      //   'plsignore',
+      //   "JAMstack or 'Pilha de Atolamento'? Let Your User Decide",
+      //   'i18n in Gatsby + Netlify'
+      // ]
+    ],
+    // false
+    true
+  )
+})()
